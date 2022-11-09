66e73a3a00f34d91a66f36ce2486e0f5acb21fbe
Nirma <nirmab71@gmail.com>
2022-11-09 18:59:17 +0530

    Completed

---
+++ Module4/mod4_script.js
@@ -0,0 +1,13 @@
+((target) => {
+	const byeSpeaker = new Object();
+	const speakWord = 'Good Bye';
+
+	byeSpeaker.speak = (name) => console.log(`${speakWord} ${name}`);
+
+	target.byeSpeaker = byeSpeaker;
+})(window);
+
+
+
+
+
